fc93e612e005875384726dff17e316c597077dda
David Beilis <david.beilis@genesys.com>
2014-10-04 18:28:00 -0400

    DB - directory structure, added login form, cleaned css

---
+++ app/chat_widget/transport/chat_transport_websocket.js
@@ -0,0 +1,51 @@
+var notifications = {};
+var ws;
+connect();
+
+function connect() {
+	var target = 'wss://genesysvoice.com:8080/gcfd/websockets/messaging';
+	if ('WebSocket' in window) {
+		ws = new WebSocket(target);
+	} else {
+		console.log('WebSocket is not supported by this browser.');
+		return;
+	}
+	ws.onopen = function() {
+		console.log('Info: WebSocket connection opened.');
+	};
+	ws.onmessage = function(event) {
+		console.log('Received: ' + event.data);
+		sendNotification(event.data);
+	};
+	ws.onclose = function() {
+		console.log('Info: WebSocket connection closed.');
+	};
+}
+
+function disconnect() {
+	if (ws != null) {
+		ws.close();
+		ws = null;
+	}
+}
+
+function getNotificationId(fullPrefName) {
+	var id = Math.floor(Math.random() * 9007199254740992) + 1;
+	var idStr = id.toString();
+	notifications[idStr] = fullPrefName;
+	return idStr;
+}
+
+function sendNotification(msg) {
+	var json = JSON.parse(msg);
+	chrome.notifications.create(getNotificationId(json.fullPrefName), {
+		title : 'New email',
+		iconUrl : 'icon_128.png',
+		type : 'basic',
+		message : json.message
+	}, creationCallback);
+}
+
+function creationCallback(notID) {
+	console.log("Succesfully created " + notID + " notification");
+}
---
+++ app/main/index.js
@@ -0,0 +1,18 @@
+// Create instance of chat widget
+
+// HTTC Transport Version
+var oChatUI = new GenesysChatUI($, $("#chat_panel"),
+	Transport_REST_HTTC, {
+		id : "515a4376-ac30-4ed2-801f-a876c0d56c93",
+		dataURL : "https://genesysvoice.com:8080/gcfd/servlets/chats/api/v2/chats/",
+		context : "demo"
+	}
+
+	// Transport_WebSocket, {
+	// 	id : "",
+	// 	dataURL : "wss://genesysvoice.com:8080/gcfd/websockets/messaging",
+	// 	context : "demo"
+	// }
+);
+
+oChatUI.startSession();
