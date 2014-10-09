b35aa561868d12cf5802146e342bb1b46a91ae0f
David Beilis <david.beilis@genesys.com>
2014-10-08 22:39:47 -0400

    DB - Added web socket reconnect on broken connection, message parsing and rendering, chat auth negations optimization

--- app/modules/chat/chat-view.js
+++ app/modules/chat/chat-view.js
@@ -15,22 +15,24 @@ angular.module('chat',[])
     .controller('ChatController', ['$scope', '$log', '$timeout', 'chatWebsocketTransportService', 
         function($scope, $log, $timeout, chatWebsocketTransportService) {
 
+            $scope.messages = [];
+
             $scope.error = null;
+
+            $scope.sendMessage = function () {
+                this.sendChatMessage(transport, $scope.messageText);
+                $scope.messageText = "";
+            }.bind(this);
+
+            $scope.disconnect = function() {
+                $log.info("Chat is disconnecting...");
+                chatWebsocketTransportService.disconnect(transport);
+            }
 
             this.sendAuthToken = function(transport, token) {
                 chatWebsocketTransportService.sendMessage(transport, {
                     "type":"token","message":{"value":token}
                 });
-            };
-
-            this.sendChatRequest = function(transport, nickname, subject) {
-                chatWebsocketTransportService.sendMessage(transport, {
-                    "type": "chat_request","message": {
-                        "operationName": "chat request",
-                        "nickname": nickname,
-                        "subject": subject
-                    }
-                })
             };
 
             this.sendChatMessage = function(transport, text) {
@@ -42,6 +44,55 @@ angular.module('chat',[])
                 })
             };
 
+            this.renderEvent = function(event) {
+
+                var data = JSON.parse(event.data);
+
+                if (data && data.type === 'chat_server_message') {
+                    if (data.message && data.message.participant && data.message.text) {
+                        var side = 'left';
+                        if (data.message.participant.type === 'Customer') {
+                            side = 'right';
+                        }
+
+                        $scope.messages.push({
+                            avatar: "images/avatar.png",
+                            text: data.message.text,
+                            side: side
+                        });
+                        $scope.$apply();
+
+                        // Animate
+                        $("#viewport-content").animate({
+                            bottom: $("#viewport-content").height() - $("#viewport").height()
+                        }, 250);
+                    }
+                }
+
+            };
+
+            this.connect = function(url) {
+                $log.info("Starting websocket connection to " + url);
+                var transport = chatWebsocketTransportService.connect(url,
+                    {
+                        'onopen' : function(event) {
+                            $log.info("Connection is established. Sending token...");
+                            this.sendAuthToken(transport, $scope.config.token);
+                        }.bind(this),
+                        'onmessage' : function(event) {
+                            this.renderEvent(event);
+                        }.bind(this),
+                        'onclose' : function(event) {
+                            this.connect(url);
+                        },
+                        'onerror' : function(event) {
+                            $scope.error = event;
+                        }
+                    });
+
+                return transport;
+            }.bind(this);
+
             if (!$scope.config || !$scope.config.webSocketAddr || !$scope.config.token) {
                 $log.warn("Invalid websocket configuration " + $scope.config);
                 $scope.error = "Invalid websocket configuration. Please contact administrator.";
@@ -49,88 +100,7 @@ angular.module('chat',[])
             }
 
             var url = $scope.config.webSocketAddr + '/gcfd/websockets/messaging';
-
-            var transport = chatWebsocketTransportService.connect(url,
-                {
-                    'onopen' : function(event) {
-                        $log.info("Connection is established. Sending token...");
-                        this.sendAuthToken(transport, $scope.config.token);
-                        $timeout(function() {
-                            this.sendChatRequest(transport, 'nickname', 'subject');
-                        }.bind(this), 10);
-                    }.bind(this),
-                    'onmessage' : function(event) {
-                        // TODO...
-                    },
-                    'onclose' : function(event) {
-
-                    },
-                    'onerror' : function(event) {
-                        $scope.error = event;
-                    }
-                });
-
-            if (!transport) {
-                // TODO: failed to create 
-            } else {
-
-                $scope.sendMessage = function () {
-                    this.sendChatMessage(transport, $scope.messageText);
-                    $scope.messageText = "";
-                }.bind(this);
-
-                $scope.disconnect = function() {
-                    $log.info("Chat is disconnecting...");
-                    chatWebsocketTransportService.disconnect(transport);
-                }
-
-                // var side = 'left';
-
-                // // Messages, client info & sending
-                // $scope.messages = [];
-
-                // Occurs when we receive chat messages
-                // server.ngChatMessagesInform = function (p) {
-                //     $scope.messages.push({
-                //         avatar: "data:image/png;base64," + p.avatar.toBase64(),
-                //         text: p.message,
-                //         side: side
-                //     });
-                //     $scope.$apply();
-
-                //     // Animate
-                //     $("#viewport-content").animate({
-                //         bottom: $("#viewport-content").height() - $("#viewport").height()
-                //     }, 250);
-
-                //     // flip the side
-                //     side = side == 'left' ? 'right' : 'left';
-                // };
-
-                var notifications;
-
-                function getNotificationId(fullPrefName) {
-                    var id = Math.floor(Math.random() * 9007199254740992) + 1;
-                    var idStr = id.toString();
-                    notifications[idStr] = fullPrefName;
-                    return idStr;
-                }
-
-                this.sendNotification = function(msg) {
-                    var json = JSON.parse(msg);
-                    chrome.notifications.create(getNotificationId(json.fullPrefName), {
-                        title : 'New email',
-                        iconUrl : 'icon_128.png',
-                        type : 'basic',
-                        message : json.message
-                    }, creationCallback);
-                }
-
-                function creationCallback(notID) {
-                    console.log("Succesfully created " + notID + " notification");
-                }
-
-            }
+            var transport = this.connect(url);
 
         }]
     ) 
